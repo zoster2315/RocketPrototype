90a15902dbe4e38903d5054ace16e224304f82f9
zoster2315 <zoster2315@gmail.com>
2021-09-12 22:52:24 +0400

    Созданы ракета, старт и финиш, препядствие.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float ThrustSpeed = 1000;
+    [SerializeField]
+    float RotationSpeed = 30;
+    Rigidbody ThisRigitbody = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThisRigitbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ThisRigitbody.AddRelativeForce(Vector3.up * ThrustSpeed * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    { 
+        var right = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        var left = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+
+        if (right && !left)
+        {
+            Rotate(-RotationSpeed);
+        }
+        else if (left && !right)
+        {
+            Rotate(RotationSpeed);
+        }
+        else if (left && right)
+        {
+            Debug.Log("Cant rotate conflict comands");
+        }
+
+    }
+
+    private void Rotate(float rotationSpeed)
+    {
+        ThisRigitbody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        ThisRigitbody.freezeRotation = false;
+    }
+}

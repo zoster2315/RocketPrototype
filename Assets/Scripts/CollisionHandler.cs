a45ba3e35f022b2e7225a94fa61833f50d808344
zoster2315 <zoster2315@gmail.com>
2021-09-16 22:09:39 +0400

    Добавлена ещё сцена. Переходы между уровянми. Перезагрузка уровня, при падении.

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField]
+    float LoadLevelDelay = 1f;
+    private void OnCollisionEnter(Collision collision)
+    {
+         switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly.");
+                break;
+            case "Finish":
+                Debug.Log("Congrads, yo, you finished!");
+                StartSuccessSequence();
+                break;
+            default:
+                Debug.Log("Sorry, you blew up!");
+                //Invoke("ReloadLevel", 1.0f);
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void StartCrashSequence()
+    {
+        // todo add SFX opon crash
+        // todo add particle effect upon crash
+        DisableControl();
+        Invoke("ReloadLevel", LoadLevelDelay);
+        //ReloadLevel();
+    }
+
+    void StartSuccessSequence()
+    {
+        // todo add SFX opon success
+        // todo add particle effect upon success
+        DisableControl();
+        Invoke("LoadNextLevel", LoadLevelDelay);
+    }
+
+    void DisableControl()
+    {
+        Movement move = GetComponent<Movement>();
+        move.enabled = false;
+    }
+
+    void ReloadLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        var nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+            nextSceneIndex = 0;
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+}
